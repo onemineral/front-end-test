3f4257224e83a2170475055c51edee9abb4b96d8
Sílvia Baptista <silviabaptista18@gmail.com>
2020-08-03 12:12:40 +0100

    Footer & Header components

--- src/components/includes/Header.vue
+++ src/components/includes/Header.vue
@@ -1,6 +1,6 @@
 <template>
-  <div>
-    <h1>this is the header</h1>
+  <div id="header">
+    <h1>Breathtaking beauty</h1>
   </div>
 </template>
 
@@ -10,4 +10,17 @@ export default {};
 </script>
 
 <style lang="scss" scoped>
+#header {
+  max-width: 1440px;
+  margin: auto;
+
+  h1 {
+    font-size: 80px;
+    line-height: 80px;
+    letter-spacing: -2.08px;
+    text-align: center;
+    color: #2e2b30;
+    font-family: "SC-Reg";
+  }
+}
 </style>
